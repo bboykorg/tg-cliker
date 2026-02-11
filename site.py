57b4ff3d199ff6fe1c320c84f283d4ebc64faab8
bboykorg <bboy.korg@gmail.com>
2026-02-11 19:18:59 +0400

    Update site.py

--- site.py
+++ site.py
@@ -39,9 +39,8 @@ def info(user_id):
         "id_improvements": result2[0],
         "add_improvements": result2[2],
         "cost_improvements": result2[1],
-        "bot": result2[8],
+        "bot": result2[7],
         "cost_bot": cost_bot
-
     }
 
 
@@ -144,20 +143,13 @@ def index():
     else:
         with get_db_connection() as conn:
             with conn.cursor() as cursor:
-                cursor.execute("SELECT \"ID_user\" FROM \"score\" WHERE \"ID_user\" = %s",
+                cursor.execute("SELECT \"score\", \"ID_improvements\" FROM \"score\" WHERE \"ID_user\" = %s",
                                (user_id,))
-                res = cursor.fetchone()
-                if res is None:
-                     cursor.execute("INSERT INTO \"score\" (\"score\", \"ID_user\", \"ID_improvements\", \"bot\", \"energy_lvl\", \"energy\") VALUES (5000, %s, 1, 0, 1, 500)",
-                                    (user_id,))
+                result = cursor.fetchone()
+                if result:
+                    click_count = result[0]
                 else:
-                    cursor.execute("SELECT \"score\", \"ID_improvements\" FROM \"score\" WHERE \"ID_user\" = %s",
-                                   (user_id,))
-                    result = cursor.fetchone()
-                    if result:
-                        click_count = result[0]
-                    else:
-                        click_count = 1
+                    click_count = 1
 
     return render_template("index.html", click_count=click_count)
 
